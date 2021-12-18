f9df63fca8350080b4f0091b7c7bc9ae23c83942
asengin <worksmail2017@gmail.com>
2021-12-18 03:13:30 +0300

    Добавьте файлы проекта.

---
+++ 1_Angle/Angles.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Angle
+{
+    class Angle
+    {
+        int grad;
+        int min;
+        int sec;
+
+        public int Grad
+        {
+            set
+            {
+                if (value > 360) // Как можно сократить данный блок? switch case не смог применить, потому что case требует int и не поддерживает диапазон
+                    grad = value % 360;
+                if (value >= 0 && value <= 360)
+                    grad = value;
+                if (value < 0 && value >= -360)
+                    grad = 360 + value;
+                if (value < -360)
+                    grad = 360 + value % 360;
+
+            }
+            get { return grad; }
+        }
+        public int Min
+        {
+            set
+            {
+                if (value >= 0 && value < 60)
+                    min = value;
+                else
+                {
+                    min = 0;
+                    Console.WriteLine($"Значение минут должно находиться в диапазоне от 0 до 60. Присвоено значение по умолчанию: {min}");
+                }
+            }
+            get { return min; }
+        }
+        public int Sec
+        {
+            set
+            {
+                if (value >= 0 && value < 60)
+                    sec = value;
+                else
+                {
+                    sec = 0;
+                    Console.WriteLine($"Значение секунд должно находиться в диапазоне от 0 до 60. Присвоено значение по умолчанию: {min}");
+                }
+            }
+            get { return sec; }
+        }
+
+        public Angle(int grad, int min, int sec) //Конструктор
+        {
+            Grad = grad;
+            Min = min;
+            Sec = sec;
+        }
+
+        public double ToRadians()
+        {
+            double radians = (Grad + Min / 60 + Sec / 3600) * Math.PI / 180;
+            return radians;
+        }
+    }
+}
---
+++ 1_Angle/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Angle
+{
+    class Program
+    {
+        /*
+         * Угол задан с помощью целочисленных значений gradus - градусов, min - угловых минут, sec - угловых секунд. Реализовать класс, 
+         * в котором указанные значения представлены в виде свойств. Для свойств предусмотреть проверку корректности данных. Класс должен 
+         * содержать конструктор для установки начальных значений, а также метод ToRadians для перевода угла в радианы. Создать объект на 
+         * основе разработанного класса. Осуществить использование объекта в программе.
+         * 
+         * В методе Main происходит получение исходных данных и передача их на обработку в экземпляр класса Angle firstAngle.
+         * В классе Angle созданы свойства Grad, Min, Sec с помощью которых осуществляется контроль входных параметров и доступа к ним.
+         * Конструктор позволяет на этапе создания объекта установить начальные значения.
+         * Отдельным методом в классе осуществляется перевод градусов в минуты, доступ к методу публичный для возможности вызова из вне.
+         * Далее в методе Main выводятся свойства объекта и результат работы метода ToRadians.
+         */
+        static void Main(string[] args)
+        {
+            #region Ввод данных
+            Console.Write("Введите градусы: ");
+            int grad = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите минуты: ");
+            int min = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите секунды: ");
+            int sec = Convert.ToInt32(Console.ReadLine());
+            #endregion
+            Angle firstAngle = new Angle(grad, min, sec);
+            Console.WriteLine($"Угол в градусах: {firstAngle.Grad}°{firstAngle.Min}'{firstAngle.Sec}\"");
+            Console.WriteLine($"Угол в радианах: {firstAngle.ToRadians():f4}");
+            Console.ReadKey();
+            
+        }
+    }
+}
